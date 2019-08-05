4d9ea663ecbe8e4fe943d055e829b657132d04b4
Martino Christanto Khuangga <martino.khuangga@tokopedia.com>
2019-08-05 23:29:14 +0700

    feat(handlers): delete implementation for boilerplate

--- internal/handlers_book.go
+++ internal/handlers_book.go
@@ -2,10 +2,7 @@ package internal
 
 import (
 	"bytes"
-	"encoding/json"
-	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -15,129 +12,33 @@ import (
 
 // GetBookByID a function to get a single book given it's ID
 func (h *Handler) GetBookByID(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
-	query := fmt.Sprintf("SELECT id, title, author, isbn, stock FROM books WHERE id = %s", param.ByName("bookID"))
-	rows, err := h.DB.Query(query)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	var books []Book
-
-	for rows.Next() {
-		book := Book{}
-		err := rows.Scan(
-			&book.ID,
-			&book.Title,
-			&book.Author,
-			&book.ISBN,
-			&book.Stock,
-		)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		books = append(books, book)
-	}
-
-	bytes, err := json.Marshal(books)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	renderJSON(w, bytes, http.StatusOK)
+	// TODO: Implement this. Query = SELECT id, title, author, isbn, stock FROM books WHERE id = <bookID>
 }
 
 // InsertBook a function to insert book to DB
 func (h *Handler) InsertBook(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
+	// TODO: Implement this. Query = INSERT INTO books (id, title, author, isbn, stock) VALUES (<id>, '<title>', '<author>', '<isbn>', <stock>)
 	// read json body
-	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		renderJSON(w, []byte(`
-			message: "Failed to read body"
-		`), http.StatusBadRequest)
-		return
-	}
 
 	// parse json body
-	var book Book
-	err = json.Unmarshal(body, &book)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
 	// executing insert query
-	query := fmt.Sprintf("INSERT INTO books (id, title, author, isbn, stock) VALUES (%d, '%s', '%s', '%s', %d)", book.ID, book.Title, book.Author, book.ISBN, book.Stock)
-	_, err = h.DB.Query(query)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
-	renderJSON(w, []byte(`
-	{
-		status: "success",
-		message: "Insert book success!"
-	}
-	`), http.StatusOK)
 }
 
 // EditBook a function to change book data in DB, with given params
 func (h *Handler) EditBook(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
-	bookID := param.ByName("bookID")
+	// TODO: Implement this. Query = UPDATE books SET title = '<title>', author = '<author>', isbn = '<isbn>', stock = <stock> WHERE id = <id>
 	// read json body
-	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		renderJSON(w, []byte(`
-			message: "Failed to read body"
-		`), http.StatusBadRequest)
-		return
-	}
 
 	// parse json body
-	var book Book
-	err = json.Unmarshal(body, &book)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
-	query := fmt.Sprintf("UPDATE books SET title = '%s', author = '%s', isbn = '%s', stock = %d WHERE id = %s", book.Title, book.Author, book.ISBN, book.Stock, bookID)
-	_, err = h.DB.Query(query)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	renderJSON(w, []byte(`
-	{
-		status: "success",
-		message: "Update book success!"
-	}
-	`), http.StatusOK)
+	// executing update query
 }
 
 // DeleteBookByID a function to remove book data from DB, given bookID
 func (h *Handler) DeleteBookByID(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
-	bookID := param.ByName("bookID")
-
-	query := fmt.Sprintf("DELETE FROM books WHERE id = %s", bookID)
-	_, err := h.DB.Exec(query)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	renderJSON(w, []byte(`
-	{
-		status: "success",
-		message: "Delete book success!"
-	}
-	`), http.StatusOK)
+	// TODO: implement this. Query = DELETE FROM books WHERE id = <id>
 }
 
 // InsertMultipleBooks a function to insert multiple book data, given file of books data
@@ -162,24 +63,14 @@ func (h *Handler) InsertMultipleBooks(w http.ResponseWriter, r *http.Request, pa
 	// copy file to buffer
 	io.Copy(&buffer, file)
 
-	contents := buffer.String()
+	// TODO: uncomment this when implementing
+	// contents := buffer.String()
 
 	// Split contents to rows
-	rows := strings.Split(contents, "\n")
-	for i, row := range rows {
-		// skip title
-		if i == 0 {
-			continue
-		}
-		// Split rows to column
-		columns := strings.Split(row, ",")
-		query := fmt.Sprintf("INSERT INTO books (id, title, author, isbn, stock) VALUES (%s, '%s', '%s', '%s', %s)", columns[0], columns[1], columns[2], columns[3], columns[4])
-		_, err = h.DB.Query(query)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-	}
+	// TODO: uncomment this when implementing
+	// rows := strings.Split(contents, "\n")
+
+	// TODO: iterate csv rows here.
 
 	buffer.Reset()
 
@@ -193,63 +84,18 @@ func (h *Handler) InsertMultipleBooks(w http.ResponseWriter, r *http.Request, pa
 
 // LendBook a function to record book lending in DB and update book stock in book tables
 func (h *Handler) LendBook(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
-	bookID := param.ByName("bookID")
-	bookStockQuery := fmt.Sprintf("SELECT stock FROM books WHERE id = %s", bookID)
+	// TODO: implement this.
+	// Get stock query = SELECT stock FROM books WHERE id = <bookID>
+	// Insert Book Lending query = INSERT INTO lend (user_id, book_id) VALUES (<userID>, <bookID>)
+	// Update stock query = UPDATE books SET stock = <newStock> WHERE id = <bookID>
 
 	// Read userID
-	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		renderJSON(w, []byte(`
-			message: "Failed to read body"
-		`), http.StatusBadRequest)
-		return
-	}
 
 	// parse json body
-	var request LendRequest
-	err = json.Unmarshal(body, &request)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
 	// Get book stock from DB
-	rows, err := h.DB.Query(bookStockQuery)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	var bookStock int
-	for rows.Next() {
-		err := rows.Scan(&bookStock)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-	}
 
 	// Insert Book to Lend tables
-	insertBookLendingQuery := fmt.Sprintf("INSERT INTO lend (user_id, book_id) VALUES (%d, %s)", request.UserID, bookID)
-	_, err = h.DB.Query(insertBookLendingQuery)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
 	// Update Book stock query
-	updateStockQuery := fmt.Sprintf("UPDATE books SET stock = %d WHERE id = %s", (bookStock - 1), bookID)
-	_, err = h.DB.Query(updateStockQuery)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	renderJSON(w, []byte(`
-	{
-		status: "success",
-		message: "Your order has been recorded!"
-	}
-	`), http.StatusOK)
 }
--- internal/handlers_user.go
+++ internal/handlers_user.go
@@ -1,10 +1,6 @@
 package internal
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -12,124 +8,28 @@ import (
 
 // GetUserByID a method to get user given userID params in URL
 func (h *Handler) GetUserByID(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
-	query := fmt.Sprintf("SELECT * FROM users WHERE id = %s", param.ByName("userID"))
-	rows, err := h.DB.Query(query)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	var users []User
-
-	for rows.Next() {
-		user := User{}
-		err := rows.Scan(
-			&user.ID,
-			&user.Name,
-		)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		users = append(users, user)
-	}
-
-	bytes, err := json.Marshal(users)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	renderJSON(w, bytes, http.StatusOK)
+	// TODO: implement this. Query = SELECT * FROM users WHERE id = <userID>
 }
 
 // InsertUser a function to insert user data (id, name) to DB
 func (h *Handler) InsertUser(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
+	// TODO: implement this. Query = INSERT INTO users (id, name) VALUES (<userID>, '<name>')
 	// read json body
-	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		renderJSON(w, []byte(`
-			message: "Failed to read body"
-		`), http.StatusBadRequest)
-		return
-	}
 
 	// parse json body
-	var user User
-	err = json.Unmarshal(body, &user)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
 	// executing insert query
-	query := fmt.Sprintf("INSERT INTO users (id, name) VALUES (%d, '%s')", user.ID, user.Name)
-	_, err = h.DB.Query(query)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	renderJSON(w, []byte(`
-	{
-		status: "success",
-		message: "Insert user success!"
-	}
-	`), http.StatusOK)
 }
 
 // EditUserByID a function to change user data (name) in DB with given params (id, name)
 func (h *Handler) EditUserByID(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
-	userID := param.ByName("userID")
+	// TODO: implement this. Query = UPDATE users SET name = '<name>' WHERE id = <userID>
 	// read json body
-	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		renderJSON(w, []byte(`
-			message: "Failed to read body"
-		`), http.StatusBadRequest)
-		return
-	}
 
 	// parse json body
-	var user User
-	err = json.Unmarshal(body, &user)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	query := fmt.Sprintf("UPDATE users SET name = '%s' WHERE id = %s", user.Name, userID)
-	_, err = h.DB.Query(query)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	renderJSON(w, []byte(`
-	{
-		status: "success",
-		message: "Update user success!"
-	}
-	`), http.StatusOK)
 }
 
 // DeleteUserByID a function to remove user data from DB given the userID
 func (h *Handler) DeleteUserByID(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
-	userID := param.ByName("userID")
-
-	query := fmt.Sprintf("DELETE FROM users WHERE id = %s", userID)
-	_, err := h.DB.Exec(query)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	renderJSON(w, []byte(`
-	{
-		status: "success",
-		message: "Delete user success!"
-	}
-	`), http.StatusOK)
+	// TODO: implement this. Query = DELETE FROM users WHERE id = <userID>
 }
